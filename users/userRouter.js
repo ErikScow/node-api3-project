7cd436a035fafa38313314fb7b506e82ba0598f9
Erik Scow <erikscow@gmail.com>
2020-05-12 22:46:03 -0700

    Added all get handlers implementing custom middleware.

--- users/userRouter.js
+++ users/userRouter.js
@@ -11,15 +11,32 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
 });
 
 router.get('/', (req, res) => {
+  userData
+    .get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      console.log('DB error at get`/`:', error)
+      res.status(500).json({ error: 'couldnt get user data from database'})
+    })
   
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  
+  res.status(200).json(req.user)
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
-  
+  userData
+    .getUserPosts(req.user.id)
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(error => {
+      console.log('DB error at get`/:id/posts`:', error)
+      res.status(500).json({ error: 'couldnt get post data from database'})
+    })
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
