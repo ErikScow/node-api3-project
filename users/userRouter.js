751e5584d0603567cdc6459f50736c2599e2deee
Erik Scow <erikscow@gmail.com>
2020-07-14 18:49:59 -0700

    Wrote out validatePost middleware.

--- users/userRouter.js
+++ users/userRouter.js
@@ -52,11 +52,23 @@ function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({message: 'missing user data'})
+  } else if (!req.body.name) {
+    res.status(400).json({message: 'missing required name field'})
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({message: 'missing poost data'})
+  } else if (!req.body.text) {
+    res.status(400).json({message: 'missing required text field'})
+  } else {
+    next()
+  }
 }
 
 module.exports = router;
